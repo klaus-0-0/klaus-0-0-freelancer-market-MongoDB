31c448bcb5332e16aa530c7a4fb90155cd5ff913
klaus-0-0 <jin0667666@gmail.com>
2026-01-14 20:34:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
+  plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: true,
+    allowedHosts: ['klaus-0-0-freelancer-market.onrender.com'],
+  },
 })
